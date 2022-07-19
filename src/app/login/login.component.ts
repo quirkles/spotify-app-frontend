a9d46bc599d2c33faf82dcbd456d08b6dfa3b7d9
quirkles <alexquirk@joinsherpa.com>
2022-07-19 11:14:02 -0400

    point to backend in prod

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
+
 export class LoginComponent {
-  loginUrl = 'http://localhost:3030/login';
+  loginUrl = `${environment.spotifyApiUrl}/login`;
 }
