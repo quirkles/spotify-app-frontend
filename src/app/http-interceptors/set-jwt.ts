e10e28ec7ef942fa95ed3402381576594a1b6e2f
quirkles <alexquirk@joinsherpa.com>
2022-07-20 10:17:37 -0400

    create interceptor

---
+++ src/app/http-interceptors/set-jwt.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse
+} from '@angular/common/http';
+
+import {finalize, Observable, tap} from 'rxjs';
+
+/** Pass untouched request through to the next request handler. */
+@Injectable()
+export class NoopInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req)
+      .pipe(
+        tap({
+          // Succeeds when there is a response; ignore other events
+          next: (event) => {
+            if(event instanceof HttpResponse) {
+              console.log("####\n")
+              console.log(event.headers)
+              console.log("####\n")
+            }
+          },
+          // Operation failed; error is an HttpErrorResponse
+        }),
+        // Log when response observable either completes or errors
+      );
+  }
+}
